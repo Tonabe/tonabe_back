06e9ac2babd27b61af1f9d99dd5824241552ef0b
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2024-10-28 20:42:01 -0300

    feat: initial config server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
 import *  as dotenv from 'dotenv'
+import express from 'express'
 
 dotenv.config()
 
 const PORT = process.env.PORT
+const app = express()
+
+app.get('/teste_endpoint', (req, res) => {
+    res.send("Olá! Você está na tela de teste de endpoint! :)")
+})
+
+app.listen(PORT, () => {
+    console.log(`🔥 | Servidor rodando em http://localhost:${PORT}`)
+})
