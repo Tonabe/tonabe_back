48744436ef9463fc82242448b2071b55adae3fd9
Vitor Zambonini <166245151+Vitor-Zamba@users.noreply.github.com>
2024-11-02 14:11:23 -0300

    Update meta.route.ts
    Adição das validações (middleware) na criação e atualização das metas.

--- src/routes/meta.route.ts
+++ src/routes/meta.route.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { createMeta, findAllMetas, findMetaByData, findMetaById, findMetaByIdProduto, deleteMeta, updateMeta } from "../controllers/meta.controller";
+import { validate } from '../middlewares/validate.middleware'
+import { CreateMetaDto, UpdateMetaDto } from '../dtos/meta.dto'
 
-const router = Router()
-
+router.post('/', validate(CreateMetaDto), createMeta) 
+router.patch('/:id', validate(UpdateMetaDto), updateMeta) 
 router.get('/', findAllMetas)
 router.get('/:id', findMetaByData)
 router.get('/:id', findMetaByIdProduto)
-router.post('/', createMeta)
 router.get('/:id', findMetaById)
 router.delete('/:id', deleteMeta)
-router.patch('/:id', updateMeta)
 
 export default router
