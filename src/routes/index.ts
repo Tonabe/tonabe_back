fe7be3d8fdb24321d5f3da0fff2da3d384df32b3
Vitor Zambonini <166245151+Vitor-Zamba@users.noreply.github.com>
2024-11-02 13:44:09 -0300

    Update index.ts
    linha relacionada a metas adicionada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import servicoRoutes from './servico.route'
+import metaRoutes from './meta.route'
 import produtoRouter from "./produto.route";
 
 const router = Router();
 
 router.use('/servicos', servicoRoutes)
 router.use('/produto', produtoRouter)
+router.use('/meta', metaRouter)
 
 export default router
