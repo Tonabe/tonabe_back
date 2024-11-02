b3e4041840f526f9f8053a4b60674c8c7ed8e8c5
Vitor Zambonini <166245151+Vitor-Zamba@users.noreply.github.com>
2024-11-02 13:35:42 -0300

    Create meta.controller.ts
    controller da Meta criado

---
+++ src/controllers/meta.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { createMetaService, deleteMetaService, findAllMetasService, findMetaByIdService, findMetaByIdProdutoService, findMetaByDataService } from "../services/meta.service";
+
+export const createMeta = async (req: Request, res: Response)=> {
+    try {
+        const meta = await createMetaService(req.body)
+        return res.status(201).json({"Meta": meta})
+    } catch (error) {
+        return res.status(400).json({"error": error})
+    }
+}
+
+export const findMetaById = async (req: Request, res: Response) => {
+    try{
+        const meta = await findMetaByIdService(Number(req.params.id))
+        return res.status(200).json({"Meta": meta})
+    }catch(error){
+        return res.status(400).json({"error": error})
+    }
+}
+
+export const findAllMetas = async (req: Request, res: Response) => {
+    const metas = await findAllMetasService()
+    return res.status(200).json({"Metas": netas})
+}
+
+export const findMetaByIdProduto = async (req: Request, res: Response) => {
+    try{
+        const meta = await findMetaByIdProdutoService(Number(req.params.id))
+        return res.status(200).json({"Meta": meta})
+    }catch(error){
+        return res.status(400).json({"error": error})
+    }
+}
+
+export const findMetaByData = async (req: Request, res: Response) => {
+    try{
+        const meta = await findMetaByDataService(Date(req.params.id))
+        return res.status(200).json({"Meta": meta})
+    }catch(error){
+        return res.status(400).json({"error": error})
+    }
+}
+
+export const deleteMeta = async (req: Request, res: Response) => {
+    try{
+        await deleteMetaService(Number(req.params.id))
+        return res.status(204).send()
+    } catch(error){
+        return res.status(400).json({"error": error})
+    }
+}
