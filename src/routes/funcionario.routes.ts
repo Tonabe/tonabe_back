b59fe3072e6b996b5bc9f661eb31c5f4565c6d73
Taina Gomes <tainacristiane6@gmail.com>
2024-11-02 22:56:55 -0300

    feat: Criado rota funcionario

---
+++ src/routes/funcionario.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express' // Importa o Router do Express
+import { createFuncionario, updateFuncionario, deletarFuncionario, findAllFuncionarioController } from '../controllers/functionario.controller' // Importa os métodos do controlador
+
+const router = Router() // Cria uma instância do Router
+
+router.post('/', createFuncionario) // Define a rota para criar um usuário
+router.get('/', findAllFuncionarioController) // Define a rota para buscar todos os usuários
+router.patch('/:id', updateFuncionario) // Define a rota para atualizar um usuário
+router.delete('/:id', deletarFuncionario) // define rota para deletar
+
+export default router // Exporta o router
