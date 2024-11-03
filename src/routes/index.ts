7c7927039cf5a4d8fba2c0a5fab958f55000628e
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2024-11-02 23:25:43 -0300

    Revert "Alteracoes taina"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,11 @@ import { Router } from "express"
 import servicoRoutes from './servico.route'
 import metaRoutes from './meta.route'
 import produtoRouter from "./produto.route";
-import funcionarioRouter from "./funcionario.route";
 
 const router = Router();
 
 router.use('/servicos', servicoRoutes)
 router.use('/produto', produtoRouter)
 router.use('/meta', metaRouter)
-router.use('/funcionario', funcionarioRouter)
-
 
 export default router
