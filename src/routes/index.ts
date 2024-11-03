163058275f4e6bad7b5134f77f7fad496b8c3051
Taina Gomes <tainacristiane6@gmail.com>
2024-11-02 23:53:11 -0300

    feat: add rota global de funcionario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,14 @@ import { Router } from "express"
 import servicoRoutes from './servico.route'
 import metaRoutes from './meta.route'
 import produtoRouter from "./produto.route";
+import funcionarioRoutes from './funcionario.route'
 
 const router = Router();
 
 router.use('/servicos', servicoRoutes)
 router.use('/produto', produtoRouter)
 router.use('/meta', metaRouter)
+router.use('/funcionario', funcionarioRoutes)
+
 
 export default router
