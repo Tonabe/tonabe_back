70e2c1619b97020d35c65bd742726f1b5c938611
Vitor Zambonini <166245151+Vitor-Zamba@users.noreply.github.com>
2024-11-06 13:28:47 -0300

    Update server.ts
    Adições relacionadas a documentação pelo swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import *  as dotenv from 'dotenv'
 import express from 'express'
 import routes from './routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 const app = express()
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(express.json())
 
